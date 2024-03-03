5d3a5549dcdf57c727046f387b351f48fd55ffcb
nguyentrunghieu297 <trunghieunguyen2729@gmail.com>
2024-03-03 23:56:55 +0700

    some new feature in form

--- src/pages/UserProfile.tsx
+++ src/pages/UserProfile.tsx
@@ -1,7 +1,10 @@
 import avatar from "../assets/img/user-img.jpg";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { FieldError, SubmitHandler, useForm } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+
+type FieldName = "firstName" | "lastName" | "email" | "phoneNumber" | "address" | "bio";
 
 const schema = z.object({
     firstName: z
@@ -21,23 +24,23 @@ const schema = z.object({
             message: "Last name is too long",
         }),
     email: z.string().email({ message: "Email is not valid" }),
-    phoneNumber: z.string().min(10, { message: "Phone number is not valid" }),
-    address: z.string(),
+    phoneNumber: z.string().regex(/(84|0[3|5|7|8|9])+([0-9]{8})\b/, {
+        message: "Phone number is not valid",
+    }),
+    address: z.string().nonempty({ message: "Address is required" }),
     bio: z.string(),
 });
 
 type FormValues = z.infer<typeof schema>;
 
-// type FormValues = {
-//     firstName: string;
-//     lastName: string;
-//     email: string;
-//     phoneNumber: string;
-//     address: string;
-//     bio: string;
-// };
-
 export default function UserProfile() {
+    const [isEditing, setIsEditing] = useState(false);
+    const handleEditProfile = () => {
+        setIsEditing(true);
+    };
+    const handleSaveOrCancel = () => {
+        setIsEditing(false);
+    };
     const {
         register,
         handleSubmit,
@@ -48,7 +51,7 @@ export default function UserProfile() {
             firstName: "Nguyen",
             lastName: "Hieu",
             email: "hieuntse160479@fpt.edu.vn",
-            phoneNumber: "0888888888",
+            phoneNumber: "0819438687",
             address: "179 Nguyen Tat Thanh, Cujut, DakNong",
             bio: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam neque explicabo perspiciatis aspernatur eligendi delectus nulla maiores est soluta temporibus sed eum vitae, molestias nemo. Lorem ipsum dolor sit amet consectetur adipisicing elit.",
         },
@@ -58,11 +61,38 @@ export default function UserProfile() {
     const onSubmit: SubmitHandler<FormValues> = async (data: FormValues) => {
         try {
             await new Promise((resolve) => setTimeout(resolve, 1000));
-            // throw new Error();
             console.log(data);
         } catch (error) {
             setError("root", { type: "manual", message: "This email is already in use" });
         }
+    };
+
+    const renderInputField = (fieldName: FieldName, label: string, errorMessage?: FieldError) => {
+        const inputProps = {
+            ...register(fieldName, {
+                required: `${label} is required`,
+            }),
+            type: "text",
+            id: fieldName,
+            className:
+                "mt-1 block w-full py-2 px-3 focus:outline-none border-b border-gray-300 rounded-md",
+        };
+
+        if (errors[fieldName]) {
+            inputProps.className += " focus:border-red-500";
+        } else {
+            inputProps.className += " focus:border-gray-700";
+        }
+
+        return (
+            <div className="form-item">
+                <label htmlFor={fieldName} className="block text-sm font-medium text-gray-700">
+                    {label}
+                </label>
+                <input disabled={!isEditing} {...inputProps} />
+                {errorMessage && <div className="text-red-500 mt-2">{errorMessage.message}</div>}
+            </div>
+        );
     };
 
     return (
@@ -88,6 +118,7 @@ export default function UserProfile() {
                 </div>
             </div>
             <div className="grid grid-cols-12 gap-10 ">
+                {/* Left side of the page */}
                 <div className="col-span-5 user-desc mt-12">
                     <div className="courses-info flex justify-evenly mt-5 text-center">
                         <div className="course">
@@ -117,14 +148,19 @@ export default function UserProfile() {
                         </p>
                     </div>
                     <div className="cta flex justify-center gap-5 mt-10">
-                        <button className="cta-btn bg-sky-500 hover:bg-white hover:text-sky-500 text-white font-semibold py-4 px-8 rounded inline-flex items-center transition-colors duration-300">
-                            Edit Profile
+                        <button
+                            onClick={handleEditProfile}
+                            disabled={isEditing}
+                            className="cta-btn bg-sky-500 hover:bg-white hover:text-sky-500 text-white font-semibold py-4 px-8 rounded inline-flex items-center transition-colors duration-300"
+                        >
+                            {isEditing ? "Editing Profile..." : "Edit Profile"}
                         </button>
                         <button className="cta-btn bg-gray-50 hover:bg-gray-500 text-gray-500 hover:text-white font-semibold py-4 px-8 rounded inline-flex items-center transition-colors duration-300">
                             Upload Photo
                         </button>
                     </div>
                 </div>
+                {/* Right side of the page */}
                 <div className="col-span-7 edit-info border-l border-solid border-gray-300 pl-10">
                     <form
                         action=""
@@ -132,97 +168,20 @@ export default function UserProfile() {
                         className="mx-auto"
                         onSubmit={handleSubmit(onSubmit)}
                     >
-                        <h2 className="text-xl font-bold text-gray-800 mb-4">EDIT INFO</h2>
+                        <h2 className="text-xl font-bold text-gray-800 mb-4">EDIT PROFILE</h2>
                         {/* Name */}
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-                            <div className="form-item">
-                                <label
-                                    htmlFor="firstName"
-                                    className="block text-sm font-medium text-gray-700"
-                                >
-                                    First name
-                                </label>
-                                <input
-                                    {...register("firstName")}
-                                    type="text"
-                                    id="firstName"
-                                    className="mt-1 block w-full py-2 px-3 focus:outline-none border-b border-gray-300 focus:border-gray-700 rounded-md"
-                                />
-                                {errors.firstName && (
-                                    <div className="text-red-500">{errors.firstName.message}</div>
-                                )}
-                            </div>
-                            <div className="form-item">
-                                <label
-                                    htmlFor="lastName"
-                                    className="block text-sm font-medium text-gray-700"
-                                >
-                                    Last name
-                                </label>
-                                <input
-                                    {...register("lastName")}
-                                    type="text"
-                                    id="lastName"
-                                    className="mt-1 block w-full py-2 px-3 focus:outline-none border-b border-gray-300 focus:border-gray-700 rounded-md"
-                                />
-                                {errors.lastName && (
-                                    <div className="text-red-500">{errors.lastName.message}</div>
-                                )}
-                            </div>
+                            {renderInputField("firstName", "First name", errors.firstName)}
+                            {renderInputField("lastName", "Last name", errors.lastName)}
                         </div>
                         {/* Address */}
-                        <div className="form-item mt-4">
-                            <label
-                                htmlFor="address"
-                                className="block text-sm font-medium text-gray-700"
-                            >
-                                Address
-                            </label>
-                            <input
-                                {...register("address", {
-                                    required: "Address is required",
-                                })}
-                                type="text"
-                                id="address"
-                                className="mt-1 block w-full py-2 px-3 focus:outline-none border-b border-gray-300 focus:border-gray-700 rounded-md"
-                            />
+                        <div className="mt-4">
+                            {renderInputField("address", "Address", errors.address)}
                         </div>
                         {/* Phone and Email */}
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4">
-                            <div className="form-item">
-                                <label
-                                    htmlFor="phone"
-                                    className="block text-sm font-medium text-gray-700"
-                                >
-                                    Phone
-                                </label>
-                                <input
-                                    {...register("phoneNumber")}
-                                    type="tel"
-                                    id="phone"
-                                    className="mt-1 block w-full py-2 px-3 focus:outline-none border-b border-gray-300 focus:border-gray-700 rounded-md"
-                                />
-                                {errors.phoneNumber && (
-                                    <div className="text-red-500">{errors.phoneNumber.message}</div>
-                                )}
-                            </div>
-                            <div className="form-item">
-                                <label
-                                    htmlFor="email"
-                                    className="block text-sm font-medium text-gray-700"
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    {...register("email")}
-                                    type="email"
-                                    id="email"
-                                    className="mt-1 block w-full py-2 px-3 focus:outline-none border-b border-gray-300 focus:border-gray-700 rounded-md"
-                                />
-                                {errors.email && (
-                                    <div className="text-red-500">{errors.email.message}</div>
-                                )}
-                            </div>
+                            {renderInputField("phoneNumber", "Phone", errors.phoneNumber)}
+                            {renderInputField("email", "Email", errors.email)}
                         </div>
                         {/* Bio */}
                         <div className="form-item mt-4">
@@ -239,6 +198,8 @@ export default function UserProfile() {
                                         message: "Bio is too long",
                                     },
                                 })}
+                                disabled={!isEditing}
+                                maxLength={250}
                                 id="bio"
                                 className="mt-1 block w-full h-32 py-2 px-3 leading-normal focus:outline-none border border-gray-300 focus:border-gray-700 rounded-md"
                             ></textarea>
@@ -251,12 +212,14 @@ export default function UserProfile() {
                             )}
                             <button
                                 disabled={isSubmitting}
+                                onClick={handleSaveOrCancel}
                                 type="submit"
                                 className="inline-flex items-center justify-center w-28 px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-sky-500 hover:bg-sky-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 transition-colors duration-300"
                             >
                                 {isSubmitting ? "Loading..." : "Save"}
                             </button>
                             <button
+                                onClick={handleSaveOrCancel}
                                 type="button"
                                 className="ml-3 inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-700 transition-colors duration-300"
                             >
